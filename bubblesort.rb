7fd863d4a1d1b0056a7c241ccb06e74ee75bbe16
Kerron King <kerron@Kerrons-MacBook-Pro.local>
2019-08-26 17:53:12 -0400

    Fixed stickler errors

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,24 +1,23 @@
 # frozen_string_literal: true
+
 def bubble_sort(arr)
   n = arr.length
 
-    until n <= 1
-    new_n = 0  
+  until n <= 1
+  new_n = 0  
 
-        for i in 1..(n - 1)
-            if arr[i - 1] > arr[i]
-                holder = arr[i - 1]
-                arr[i - 1] = arr[i]
-                arr[i] = holder
-
-                new_n = i
-            end
-        end
-
-        n = new_n
+    for i in 1..(n - 1)
+      if arr[i - 1] > arr[i]
+        holder = arr[i - 1]
+        arr[i - 1] = arr[i]
+        arr[i] = holder
+        new_n = i
+      end
     end
 
-    arr
+    n = new_n
+end
+  arr
 end
 
 bubble_sort(arr)
@@ -30,23 +29,19 @@ def bubble_sort_by(arr)
 end
 
 sorter = Proc.new { |arr| 
-      n = arr.length
-      until n <= 1
-      new_n = 0  
-
-          for i in 1..(n - 1)
-              if arr[i - 1].length > arr[i].length
-                  holder = arr[i - 1]
-                  arr[i - 1] = arr[i]
-                  arr[i] = holder
-
-                  new_n = i
-              end
-          end
-
-          n = new_n
-      end 
-  }
-
+n = arr.length
+until n <= 1
+  new_n = 0
+  for i in 1..(n - 1)
+    if arr[i - 1].length > arr[i].length
+      holder = arr[i - 1]
+      arr[i - 1] = arr[i]
+      arr[i] = holder
+      new_n = i
+    end
+  end
+  n = new_n
+end 
+}
 
 bubble_sort_by(["hi","h", "hello"]) {sorter}
