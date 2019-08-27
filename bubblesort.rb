3f61b2118c8b451a8a9d68fd2d5bd191c315d10c
Kerron King <kerron@Kerrons-MacBook-Pro.local>
2019-08-27 11:53:35 -0400

    Fixing Linter errors

--- bubblesort.rb
+++ bubblesort.rb
@@ -6,7 +6,7 @@ def bubble_sort(arr)
     new_n = 0
 
     arr.each_with_index do |item, index|
-      if index > 0 && arr[index - 1] > arr[index]
+      if item && index > 0 && arr[index - 1] > arr[index]
         holder = arr[index - 1]
         arr[index - 1] = arr[index]
         arr[index] = holder
@@ -18,31 +18,30 @@ def bubble_sort(arr)
   arr
 end
 
-puts bubble_sort([45,3,12,4])
+puts bubble_sort([45, 3, 12, 4])
 
 def bubble_sort_by(arr)
   # Sort by length instead of value
   n = arr.length
-  
   until n <= 1
     new_n = 0
 
     arr.each_with_index do |item, index|
-      diff = yield arr[index-1], arr[index]
-        if diff > 0 && index > 0 && arr[index - 1].length > arr[index].length
-          holder = arr[index-1]
-          arr[index-1] = arr[index]
+      diff = yield arr[index - 1], arr[index]
+        if diff.positive? && item && index.positive? && arr[index - 1].length > arr[index].length
+          holder = arr[index - 1]
+          arr[index - 1] = arr[index]
           arr[index] = holder
           new_n = index
         end
-      end
+    end
     n = new_n
   end
   puts arr
 end
 
-arr = ["hello","hey","hi", "lollll", "woow"]
+arr = %w(hello hey hi lollll woow)
 
 bubble_sort_by(arr) do |x, y|
-x.length - y.length
+  x.length - y.length
 end
